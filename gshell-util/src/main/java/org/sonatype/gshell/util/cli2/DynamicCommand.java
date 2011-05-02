80db96b59065a847a173f00c38175489f699e37c
Rune Peter Bjørnstad <runepeter@gmail.com>
2011-05-02 23:08:24 +0200

    jallla
    Signed-off-by: Rune Peter Bjørnstad <runepeter@gmail.com>

--- gshell-util/src/main/java/org/sonatype/gshell/util/cli2/DynamicCommand.java
+++ gshell-util/src/main/java/org/sonatype/gshell/util/cli2/DynamicCommand.java
@@ -2,6 +2,9 @@ package org.sonatype.gshell.util.cli2;
 
 import java.util.Map;
 
+/**
+ * Dynamic command with arguments specified in a Map. Can be used as a basis for providing a JMX shell.
+ */
 public interface DynamicCommand
 {
     Map<String, Class<?>> getArgumentMap();
